d68ffca146cb48eab56ed150ab5a06247599d696
ErickVGusson <00001097739533sp@al.educacao.sp.gov.br>
2023-08-09 11:49:52 -0300

    Operadores Aritméticos

---
+++ Aula 02/Aula 02/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float b;
+            float a;
+            float soma;
+            float subtracao;
+            float multiplicacao;
+            float divisao;
+
+            Console.Write( "Digite o 1º número: " );
+            a = float.Parse( Console.ReadLine() );
+
+            Console.Write( "Digite o 2º número: " );
+            b = float.Parse( Console.ReadLine() );
+
+            soma = a + b ;
+            subtracao = a - b ;
+            multiplicacao = a * b ;
+            divisao = a / b ;
+            
+            Console.WriteLine( "\nA soma dos valores deu: " + soma );
+            Console.WriteLine( "A subtração dos valores deu: " + subtracao );
+            Console.WriteLine( "A multiplicação dos valores deu: " + multiplicacao );
+            Console.WriteLine( "A divisão dos valores deu: " + divisao );
+
+
+
+
+
+            //Console.WriteLine("Hello world");
+
+            Console.ReadKey();
+
+        }
+    }
+}
